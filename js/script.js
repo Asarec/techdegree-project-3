61043c3cc21dfd2554411e28f38339381c65db2e
Alfredo Garcia <Asarec@users.noreply.github.com>
2020-07-14 23:50:15 -0400

    script.js update
    Added focus on page load, and the ability to show and hide the #other-title input field based on user selection.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
+const jobRole = document.querySelector('#title');
+const otherTitleInput = document.querySelector('#other-title');
+
+// On page load focus on first input element.
 document.querySelector('input').focus();
+
+// Hide #other-title on page load.
+otherTitleInput.style.display = 'none';
+
+// addEventListener to jobRole to act on role change.
+jobRole.addEventListener('change', () => {
+    // Show or hide otherTitleInput based on user selection.
+    if ( jobRole.value === 'other' ) {
+        otherTitleInput.removeAttribute('style');
+    } else {
+        // Remove any entered text and hide input field.
+        otherTitleInput.value = '';
+        otherTitleInput.style.display = 'none';
+    }
+});
