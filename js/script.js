15b9fde77cbb887ca07b1a2a0c3e3fd253f1679e
Alfredo Garcia <Asarec@users.noreply.github.com>
2020-08-06 21:36:05 -0400

    Refactored, and updated code for Basic Info Section and T-Shirt Info Section.

--- js/script.js
+++ js/script.js
@@ -1,83 +1,65 @@
 const titleSelect = document.querySelector('#title');
 const otherTitle = document.querySelector('#other-title');
+
 const designSelect = document.querySelector('#design');
 const colorContainer = document.querySelector('#colors-js-puns');
-const colorSelect = document.querySelector('#color');
-const defaultColorOption = document.createElement('option');
-const hiddenColors = [];
 
-// Set focus on the first input element.
+// Focus first input element.
 document.querySelector('input').focus();
 
-// Initial instructions on load.
+// Hide #other-title and #colors-js-puns.
 otherTitle.style.display = 'none';
 colorContainer.style.display = 'none';
 
-// Add text content to defaultColorOption.
-defaultColorOption.textContent = 'Please select a color';
-
 // addEventListeners to form elements.
-titleSelect.addEventListener('change', () => { showHideElement(); });
-designSelect.addEventListener('change', () => { showHideElement(); });
+titleSelect.addEventListener('change', () => {
+    jobRole();
+});
 
-// Show or hide form elements based on user selection.
+designSelect.addEventListener('change', () => {
+    tShirtInfo();
+});
 
-function showHideElement() {
-    // Job Role
+// Basic Info Section.
+function jobRole() {
+    // Check user's selection for Job Role.
     if (titleSelect.value === 'other') {
-        // Show 'other' input field.
+        // Show #other-title input field.
         otherTitle.removeAttribute('style');
     } else {
-        // Hide 'other' input field and remove inputted value.
+        // Hide #other-title input field and remove any inputted value.
         otherTitle.style.display = 'none';
         otherTitle.value = '';
     }
-
-    // T-Shirt Info
-    if (designSelect.value === 'Select Theme') {
-        // Hide 'colorContainer' on 'Select Theme' option.
-        colorContainer.style.display = 'none';
-    } else if (designSelect.value === 'js puns') {
-        rotatingColors('JS Puns');
-    } else if (designSelect.value === 'heart js') {
-        rotatingColors('JS shirt only');
-    }
 }
 
-// hiddenColorOptions
-function rotatingColors(shirtTheme) {
-    // Show 'colorContainer'.
-    colorContainer.removeAttribute('style');
+// T-Shirt Info Section.
+function tShirtInfo() {
+    const colorSelect = document.querySelector('#color');
 
-    // Check if hiddenColors has values.
-    if (hiddenColors.length) {
-        // Iterate through hiddenColors.
-        for (let i = 0; i < hiddenColors.length; i++) {
-            // Add hiddenColor to colorSelect.
-            colorSelect.add(hiddenColors[i]);
+    // Show or hide #color-js-puns based on #design value.
+    designSelect.value !== 'Select Theme'
+        ? colorContainer.removeAttribute('style')
+        : (colorContainer.style.display = 'none');
 
-            // Remove added color from hiddenColor.
-            hiddenColors.shift(i);
+    // Iterate through #color and show/hide based on #design value.
+    Array.from(colorSelect).forEach(color => {
+        // Ensure elements do not disappear and multiple event launches.
+        color.removeAttribute('style');
 
-            i--;
+        // Condition check for color options.
+        if (designSelect.value === 'js puns' && color.text.includes('♥')) {
+            color.style.display = 'none';
+        } else if (designSelect.value === 'heart js' && color.text.includes('Puns')) {
+            color.style.display = 'none';
         }
-    }
+    });
 
-    // Iterate through colorSelect and hide colors based on user selection.
+    // Find first visible option and display as selectedIndex.
     for (let i = 0; i < colorSelect.length; i++) {
-        // If 'shirtTheme' is NOT present in string.
-        if (!colorSelect[i].textContent.includes(shirtTheme)) {
-            // Add true value to hiddenColors array.
-            hiddenColors.push(colorSelect[i]);
-
-            // Remove from NodeList.
-            colorSelect.remove(i);
-
-            i--;
+        if (!colorSelect[i].hasAttribute('style')) {
+            colorSelect.selectedIndex = i;
+            break;
         }
     }
-
-    // Add defaultColorOption and select it.
-    colorSelect.add(defaultColorOption, 'before');
-    colorSelect.selectedIndex = 0;
 }
