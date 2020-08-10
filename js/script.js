04b74a39c7426bbef1ce0f287878425cf80c0e28
Alfredo Garcia <Asarec@users.noreply.github.com>
2020-08-10 19:28:04 -0400

    Activity and Payment Section

--- js/script.js
+++ js/script.js
@@ -4,12 +4,31 @@ const otherTitle = document.querySelector('#other-title');
 const designSelect = document.querySelector('#design');
 const colorContainer = document.querySelector('#colors-js-puns');
 
+const activities = document.querySelector('.activities');
+
+const paymentInfo = document.querySelector('#payment');
+const paypal = document.querySelector('#paypal');
+const bitcoin = document.querySelector('#bitcoin');
+
+const registerButton = document.querySelector('button');
+
 // Focus first input element.
 document.querySelector('input').focus();
 
-// Hide #other-title and #colors-js-puns.
+// Hide some HTML elements on load.
 otherTitle.style.display = 'none';
 colorContainer.style.display = 'none';
+paypal.style.display = 'none';
+bitcoin.style.display = 'none';
+
+// Create and append new element for price total.
+const newDiv = document.createElement('div');
+newDiv.setAttribute('style', 'border-top: rgba(8, 63, 87, 0.3) 2px dashed; padding-top: 22px;');
+newDiv.innerHTML = '<b>Total: </b>$0';
+activities.after(newDiv);
+
+// Payment Info Adjustments.
+paymentInfo.remove(0);
 
 // addEventListeners to form elements.
 titleSelect.addEventListener('change', () => {
@@ -18,6 +37,18 @@ titleSelect.addEventListener('change', () => {
 
 designSelect.addEventListener('change', () => {
     tShirtInfo();
+});
+
+activities.addEventListener('change', event => {
+    activityRegister(event);
+});
+
+paymentInfo.addEventListener('change', () => {
+    paymentSection();
+});
+
+registerButton.addEventListener('click', event => {
+    formValidation(event);
 });
 
 // Basic Info Section.
@@ -44,14 +75,14 @@ function tShirtInfo() {
 
     // Iterate through #color and show/hide based on #design value.
     Array.from(colorSelect).forEach(color => {
-        // Ensure elements do not disappear and multiple event launches.
+        // Ensure elements do not disappear on multiple event launches.
         color.removeAttribute('style');
 
         // Condition check for color options.
-        if (designSelect.value === 'js puns' && color.text.includes('♥')) {
-            color.style.display = 'none';
-        } else if (designSelect.value === 'heart js' && color.text.includes('Puns')) {
-            color.style.display = 'none';
+        switch (true) {
+            case designSelect.value === 'js puns' && color.text.includes('♥'):
+            case designSelect.value === 'heart js' && color.text.includes('Puns'):
+                color.style.display = 'none';
         }
     });
 
@@ -61,5 +92,65 @@ function tShirtInfo() {
             colorSelect.selectedIndex = i;
             break;
         }
+    }
+}
+
+function activityRegister(event) {
+    const allActivities = activities.querySelectorAll('input');
+    let totalPrice = 0;
+
+    // Is checkbox ticked?
+    if (event.target.checked) {
+        // Iterate through the activities.
+        for (let i = 0; i < allActivities.length; i++) {
+            // Does the target input's schedule match but has a different name?
+            if (
+                allActivities[i].getAttribute('data-day-and-time') === event.target.getAttribute('data-day-and-time') &&
+                allActivities[i].getAttribute('name') !== event.target.getAttribute('name')
+            ) {
+                // Disable the checkbox.
+                allActivities[i].disabled = true;
+            }
+        }
+    } else {
+        for (let i = 0; i < allActivities.length; i++) {
+            // Does the target input's schedule match but has a different name?
+            if (
+                allActivities[i].getAttribute('data-day-and-time') === event.target.getAttribute('data-day-and-time') &&
+                allActivities[i].getAttribute('name') !== event.target.getAttribute('name')
+            ) {
+                // Enable the checkbox.
+                allActivities[i].disabled = false;
+            }
+        }
+    }
+
+    // Iterate through the checked boxes and add its cost to the totalPrice.
+    for (let i = 0; i < allActivities.length; i++) {
+        if (allActivities[i].checked) {
+            totalPrice += Number(allActivities[i].getAttribute('data-cost'));
+        }
+    }
+
+    // Print the total to the DOM.
+    newDiv.innerHTML = `<b>Total: </b>$${totalPrice}`;
+}
+
+function paymentSection() {
+    const creditCard = document.querySelector('#credit-card');
+
+    // Condition the shown content based on user selection.
+    if (paymentInfo.value === 'paypal') {
+        creditCard.style.display = 'none';
+        bitcoin.style.display = 'none';
+        paypal.removeAttribute('style');
+    } else if (paymentInfo.value === 'bitcoin') {
+        creditCard.style.display = 'none';
+        paypal.style.display = 'none';
+        bitcoin.removeAttribute('style');
+    } else {
+        paypal.style.display = 'none';
+        bitcoin.style.display = 'none';
+        creditCard.removeAttribute('style');
     }
 }
